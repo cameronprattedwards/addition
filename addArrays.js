f6f8f201d169392d7ac1c2a712f0d733d8bdff80
Cameron Edwards <cameron@hioscar.com>
2016-04-19 13:49:46 -0400

    Add solution

--- addArrays.js
+++ addArrays.js
@@ -1,6 +1,23 @@
 function addArrays(array1, array2) {
-	// Fill in this function so that it returns an array whose entries are the digits the sum of the numbers represented as digits in the
-	// arrays passed as arguments.
+	let output = [];
+	array1 = array1.reverse();
+	array2 = array2.reverse();
+	let carry = 0;
+
+	for (let i = 0; i < array1.length || i < array2.length; i++) {
+		const a = array1[i] || 0;
+		const b = array2[i] || 0;
+		let sum = a + b + carry;
+		carry = Math.floor(sum/10);
+		sum %= 10;
+		output.push(sum);
+	}
+
+	if (carry) {
+		output.push(carry);
+	}
+
+	return output.reverse();
 }
 
 console.log(addArrays([1,2], [1]));  // should be [1,3]
