b6c923853fb48e91cf8409d8d144b2f9631ce6a7
Cameron Edwards <cameron@hioscar.com>
2016-04-19 13:49:05 -0400

    Present problem

---
+++ addArrays.js
@@ -0,0 +1,9 @@
+function addArrays(array1, array2) {
+	// Fill in this function so that it returns an array whose entries are the digits the sum of the numbers represented as digits in the
+	// arrays passed as arguments.
+}
+
+console.log(addArrays([1,2], [1]));  // should be [1,3]
+console.log(addArrays([9,9,9], [1]));  // should be [1,0,0,0]
+console.log(addArrays([9,7,3,7], [3,4,5]));  // should be [1,0,0,8,2]
+console.log(addArrays([9,9,9], [1,1,1]));  // should be [1,1,1,0]
